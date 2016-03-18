cfb46dd074b75d395e53d8ccfdc9399880cd9ba9
Corentin DROUET <cdrouet@e3r10p1.42.fr>
2016-03-18 11:59:56 +0100

    lem-in

--- ft_fourmis.c
+++ ft_fourmis.c
@@ -6,15 +6,26 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 08:43:11 by cdrouet           #+#    #+#             */
-/*   Updated: 2016/03/18 08:52:34 by cdrouet          ###   ########.fr       */
+/*   Updated: 2016/03/18 11:59:32 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
-t_fourmis	*new_fourmis()
-
-void	init_fourmis(t_fourmis	**f, int nb_f)
+t_fourmis	*new_fourmis(int id, t_salle *name, t_path *path)
 {
+	t_fourmis	*f;
+
+	f = (t_fourmis*)malloc(sizeof(t_fourmis));
+	f->id = id;
+	f->name = name;
+	f->path = path;
+	return (f);
+}
+
+t_fourmis	*init_fourmis(int nb_f, t_salle *room, t_path *path)
+{
+	t_fourmis	*creat;
+
 	
 }
