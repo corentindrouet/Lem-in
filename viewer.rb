215e4e008b29830faeddd06f98b0260eae52a942
Corentin DROUET <cdrouet@e3r7p2.42.fr>
2016-03-25 11:55:47 +0100

    lem-in

--- viewer.rb
+++ viewer.rb
@@ -2,24 +2,63 @@
 require 'gosu'
 
 class Viewer < Gosu::Window
+	attr_accessor :x, :y, :t, :tun
+
 	def initialize
 		super 640, 480
 		self.caption = "Viewer Lem-in"
-		@image = Gosu::Image.new("viewer_src/point.png")
+		@x = 0
+		@y = 0
 	end
 	def update
 	end
-	def draw(@x, @y)
-		@image.draw(@x, @y, 0)
+	def draw
+		i = 0
+		while (t[i])
+			draw_quad(t[i][1].to_i * 20, t[i][2].to_i * 20, 0xffff8888, (t[i][1].to_i * 20) + 20, (t[i][2].to_i * 20), 0xffff8888, (t[i][1].to_i * 20), (t[i][2].to_i * 20) + 20, 0xffff8888, (t[i][1].to_i * 20) + 20, (t[i][2].to_i * 20) + 20, 0xffff8888, 0)
+			message = Gosu::Image.from_text(self, t[i][0], Gosu.default_font_name, 15)
+			message.draw((t[i][1].to_i * 20) + 5, t[i][2].to_i * 20, 0)
+			i += 1
+		end
+		i = 0
+		while (tun[i])
+			j = 0
+			while (t[j] && t[j][0] != tun[i][0])
+				j += 1
+			end
+			k = 0
+			while (t[k] && t[k][0] != tun[i][1])
+				k += 1
+			end
+			if (t[j] && t[k])
+				draw_line((t[j][1].to_i * 20) + 10, (t[j][2].to_i * 20) + 10, 0xffffffff, (t[k][1].to_i * 20) + 10, (t[k][2].to_i * 20) + 10, 0xffffffff)
+			end
+			i += 1
+		end
 	end
 end
 
 window = Viewer.new
 
 prout = gets
+tab = []
+tabtun = []
 while (prout != "\n")
-	
+	if (prout[0] != '#')
+		line = prout.split
+		if (line[1])
+			tab<<line
+		else
+			line = prout.split('-')
+			if (line[1])
+				tabtun<<line
+			end
+		end
+	end
 	prout = gets
 end
 
+				puts tabtun
+window.t = tab
+window.tun = tabtun
 window.show
