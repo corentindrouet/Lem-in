c7c69204f87247e1b4d7f9dd99b351bf1afe0e00
Corentin DROUET <cdrouet@e3r7p2.42.fr>
2016-03-25 15:07:08 +0100

    lem-in

--- viewer.rb
+++ viewer.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/ruby
 require 'gosu'
 
+class Fourmis
+	attr_accessor :act
+end
+
 class Viewer < Gosu::Window
 	attr_accessor :x, :y, :t, :tun
 
@@ -13,13 +17,6 @@ class Viewer < Gosu::Window
 	def update
 	end
 	def draw
-		i = 0
-		while (t[i])
-			draw_quad(t[i][1].to_i * 20, t[i][2].to_i * 20, 0xffff8888, (t[i][1].to_i * 20) + 20, (t[i][2].to_i * 20), 0xffff8888, (t[i][1].to_i * 20), (t[i][2].to_i * 20) + 20, 0xffff8888, (t[i][1].to_i * 20) + 20, (t[i][2].to_i * 20) + 20, 0xffff8888, 0)
-			message = Gosu::Image.from_text(self, t[i][0], Gosu.default_font_name, 15)
-			message.draw((t[i][1].to_i * 20) + 5, t[i][2].to_i * 20, 0)
-			i += 1
-		end
 		i = 0
 		while (tun[i])
 			j = 0
@@ -35,30 +32,54 @@ class Viewer < Gosu::Window
 			end
 			i += 1
 		end
+		i = 0
+		while (t[i])
+			color = 0xff00ffff
+			if (t[i] == "##start")
+				i += 1
+				color = 0xffff0000
+			end
+			if (t[i] == "##end")
+				i += 1
+				color = 0xff00ff00
+			end
+			draw_quad(t[i][1].to_i * 20, t[i][2].to_i * 20, color, (t[i][1].to_i * 20) + 20, (t[i][2].to_i * 20), color, (t[i][1].to_i * 20), (t[i][2].to_i * 20) + 20, color, (t[i][1].to_i * 20) + 20, (t[i][2].to_i * 20) + 20, color, 0)
+			message = Gosu::Image.from_text(self, t[i][0], Gosu.default_font_name, 15)
+			message.draw((t[i][1].to_i * 20) + 5, t[i][2].to_i * 20, 0)
+			i += 1
+		end
 	end
 end
 
 window = Viewer.new
 
 prout = gets
+if (prout == "Error\n")
+	puts prout
+	Process.exit!(true)
+end
+nb_f = prout.to_i
 tab = []
 tabtun = []
 while (prout != "\n")
-	if (prout[0] != '#')
-		line = prout.split
+	prout = prout.chomp("\n")
+	line = prout.split
+	if (prout[0] == '#')
+		tab<<prout
+	elsif (line[1])
+		tab<<line
+	else
+		line = prout.split('-')
 		if (line[1])
-			tab<<line
-		else
-			line = prout.split('-')
-			if (line[1])
-				tabtun<<line
-			end
+			tabtun<<line
 		end
 	end
 	prout = gets
 end
+#while (prout != nil)
+	
+#end
 
-				puts tabtun
 window.t = tab
 window.tun = tabtun
 window.show
